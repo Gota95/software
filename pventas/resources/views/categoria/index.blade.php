9b8ea822ea5cc9e4fd3f836d008a7889e48d8c4e
Gota95 <colopg4@gmail.com>
2019-10-01 16:45:31 -0600

    01102019
    Plantilla Nueva

--- pventas/resources/views/categoria/index.blade.php
+++ pventas/resources/views/categoria/index.blade.php
@@ -1,38 +1,44 @@
 @extends ('layouts.admin')
 @section ('contenido')
-<h3>  INDEX </h3> 
+<h3>  INDEX </h3>
 <div class="row">
-<div class="col-lg-8 col-md-8 col-sm8 col-xs-12">
-<h3>Listado de Categorias <a href="categoria/create"><button>Nuevo</button></a>
-@include('categoria.search')
+  <div class="col-lg-8 col-md-8 col-sm8 col-xs-12">
+    <h3>Listado de Categorias <a href="categoria/create"><button>Nuevo</button></a>
+    @include('categoria.search')
+  </div>
 </div>
-<div class="row">
-<div class="col-lg-12 col-md-12 cols-sm-12 col-xs-12">
-<div class="table-responsive">
-<table class="table table-striped table-bordered table-consenden table-hover">
-<thead>
-<th>Id</th>
-<th>Nombre </th>
-<th>Descripcion </th>
-<th>Opciones</th>
-</thead>
-@foreach($categorias as $cat)
-<tr>
-<td>{{$cat->idcategoria}}</td>
-<td>{{$cat->nombre}}</td>
-<td><{{$cat->descripcion}}/td>
-<td>
-<a href=""><button class="btn btn-info"> Editar </button></a>
-<a href=""><button class="btn btn-danger"> Eliminar </button></a>
+<div class="container-fluid">
+  <div class="col-lg-8 col-lm-12">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Data Table</h4>
+          <div class="table-responsive">
+            <table class="table table-striped table-bordered zero-configuration">
+              <thead>
+                <th>Id</th>
+                <th>Nombre </th>
+                <th>Descripcion </th>
+                <th>Opciones</th>
+              </thead>
+              @foreach($categorias as $cat)
+                <tr>
+                  <td>{{$cat->idcategoria}}</td>
+                  <td>{{$cat->nombre}}</td>
+                  <td><{{$cat->descripcion}}</td>
+                  <td>
+                  <a href=""><button class="btn btn-info"> Editar </button></a>
+                  <a href=""><button class="btn btn-danger"> Eliminar </button></a>
 
-</td>
+                </td>
 
-</tr>
-@endforeach
-</table>
-</div>
-{{$categorias->render()}}
-</div>
-</div>
+                </tr>
+              @endforeach
+            </table>
+          </div>
+          {{$categorias->render()}}
+        </div>
+      </div>
+    </div>
+  </div>
 
 @endsection
