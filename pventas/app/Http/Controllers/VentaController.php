c0f86b27cff9fbd57fe44768cce5de754488a114
Gota95 <colopg4@gmail.com>
2019-10-13 08:38:02 -0600

    13102019
    create model request and controller for the ventas

---
+++ pventas/app/Http/Controllers/VentaController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VentaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+      if($request){
+        $query= trim($request->get('searchText'));
+        $ventas=DB::table('venta as ven')
+        ->join('persona as per','ven.idcliente','=','per.idpersona')
+        ->select('ven.idventa','ven.tipo_comprobante','ven.serie_comprobante','num_comprobante',
+        'fecha_hora','impuesto','total_venta','ven.estado',DB::raw('per.nombre as nombrecliente'))
+        ->where('ven.num_comprobante','LIKE','%'.$query.'%')
+        ->orderBy('ven.idventa','asc')
+        ->paginate(7);
+
+        return view("venta.index",["ventas"=>$ventas,"searchText"=>$query]);
+      }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
