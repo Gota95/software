3dc1b47f821f3d25cd473f2989feb1584018d1af
Gota95 <colopg4@gmail.com>
2019-11-03 15:45:46 -0600

    Revert "03112019"
    This reverts commit e9dcdbf0682faf7ae2f6451b88f711cef1baff49.

--- pventas/config/app.php
+++ pventas/config/app.php
@@ -226,11 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-<<<<<<< HEAD
         'PDF' => Barryvdh\DomPDF\Facade::class,
-=======
-
->>>>>>> parent of 252d743e... 24102019
     ],
 
 ];
