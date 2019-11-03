b735ae15fdd61d6d30fc9431c23443d5d641fe6f
Gota95 <colopg4@gmail.com>
2019-11-03 15:40:10 -0600

    03112019
    update admin.blade.php

--- pventas/resources/views/layouts/admin.blade.php
+++ pventas/resources/views/layouts/admin.blade.php
@@ -45,7 +45,101 @@
             <div class="hamburger">
               <span class="toggle-icon"><i class="icon-menu"></i></span>
             </div>
+<<<<<<< HEAD
           </div>
+=======
+        </div>
+        <!--**********************************
+            Nav header end
+        ***********************************-->
+
+        <!--**********************************
+            Sidebar start
+        ***********************************-->
+
+        <div class="nk-sidebar">
+            <div class="nk-nav-scroll">
+                <ul class="metismenu" id="menu">
+                    <li class="nav-label">Menú</li>
+                    <li>
+                        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                            <i class="icon-people"></i><span class="nav-text">Registro</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="/users">Usuarios</a></li>
+                            <li><a href="/persona">Persona</a></li>
+                            <li><a href="/tipo">Tipo de persona</a></li>
+                            <!-- <li><a href="./index-2.html">Home 2</a></li> -->
+                        </ul>
+                    </li>
+                  <li>
+                        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                            <i class="icon-basket-loaded "></i><span class="nav-text">Productos</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="/categoria">Categoría</a></li>
+                            <li><a href="/articulo">Articulos</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-label">Compra/Venta</li>
+                    <li>
+                        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                            <i class="icon-bag "></i> <span class="nav-text">Ingresos</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="/ingreso">Ingreso</a></li>
+
+                        </ul>
+
+                    </li>
+                    <li>
+                        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                            <i class="icon-basket "></i><span class="nav-text">Ventas</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="/venta">Venta</a></li>
+
+                        </ul>
+                    </li>
+                    <br>
+                    <li class="nav-label">Mas opciones</li>
+                    <li>
+
+                        <a href="./app-profile.html">
+                        <i class="icon-grid  "></i><span class="nav-text">Estadisticas</span>
+                        </a>
+                        </li>
+                        <li>
+                        <a href="./app-profile.html">
+                        <i class="icon-docs"></i><span class="nav-text">Reportes</span>
+                        </a>
+                        </li>
+                        <li>
+                        <a href="./app-profile.html">
+                        <i class="icon-social-facebook "></i><span class="nav-text">Acerca de</span>
+                        </a>
+
+</li>
+
+
+
+                </ul>
+            </div>
+        </div>
+        <!--**********************************
+            Sidebar end
+        ***********************************-->
+
+        <!--**********************************
+            Content body start
+        ***********************************-->
+        <div class="content-body">
+
+
+
+                @yield('contenido')
+>>>>>>> parent of 059482c4... 28102019
 
         </div>
 
